77c535093bff9a74c9ece3f5c1bdb5747a960bd1
s3891667 <s3891667@rmit.edu.vn>
2023-06-20 11:47:59 +1000

    create a routing structure, will be implemented

--- front-end/src/app/about/page.tsx
+++ front-end/src/app/about/page.tsx
@@ -1,10 +1,8 @@
-import Image from 'next/image'
-import Nav from '../components/nav.js'
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <Nav/>
     <h1> Testing </h1>
     </main>
   )
---
+++ front-end/src/app/register/layout.tsx
@@ -0,0 +1,7 @@
+export default function Registerlayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return <section>{children}</section>
+}
