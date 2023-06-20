a897f3ae1ab8fd51d1b65a7d0856c15f9289ff7a
s3891667 <s3891667@rmit.edu.vn>
2023-06-20 20:39:32 +1000

    implement nav bar

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image'
 import Nav from '../components/nav.js'
+//import Footer from '../components/footer.js'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <Nav/>
-    <h1> Testing </h1>
-    </main>
+	<>
+	<header>
+		<Nav/>
+	</header>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    </>
   )
 }
