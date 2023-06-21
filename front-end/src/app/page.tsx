fe7e40b39ec74c11514eafa14897f6096ac0a8a4
s3891667 <s3891667@rmit.edu.vn>
2023-06-21 11:02:19 +1000

    finish nav bar + animation

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,12 +1,36 @@
-import Image from 'next/image'
+"use client";
 import Nav from '../components/nav.js'
 //import Footer from '../components/footer.js'
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+
+      if (prevScrollPos > currentScrollPos) {
+	setIsVisible(true);
+      } else {
+	setIsVisible(false);
+      }
+
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [prevScrollPos]);
   return (
 	<>
-	<header>
+	<header className={`fixed top-0 left-0 w-full bg-white shadow-lg transition duration-300 transform ${isVisible ? 'translate-y-0' : '-translate-y-full'}`}>
 		<Nav/>
+		
 	</header>
     <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
     </>
